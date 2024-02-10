2f139523d9fcaeb0d2224b1d53403c65dfdbf53f
Alexander Brown <kemikalstealth@icloud.com>
2024-02-10 03:41:19 -0600

    Added basic BCard element

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="d-flex justify-content-center align-items-center mt-5">
+    <!-- TODO: Add BCard -->
   </div>
 </template>
+
+<script setup lang="ts"></script>
---
+++ components/card/BCard.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+defineProps({
+  header: String,
+  footer: String,
+  title: String,
+  imgSrc: String,
+  imgAlt: String,
+  noBody: Boolean,
+});
+</script>
+
+<template>
+  <div class="card">
+    <img v-if="imgSrc" :src="imgSrc" :alt="imgAlt" class="card-img-top" />
+
+    <div v-if="header" class="card-header">{{ header }}</div>
+    <div v-else-if="$slots.header">
+      <slot name="header"></slot>
+    </div>
+
+    <slot v-if="noBody"></slot>
+    <div v-else class="card-body">
+      <h5 v-if="title" class="card-title">{{ title }}</h5>
+      <slot></slot>
+    </div>
+
+    <div v-if="footer" class="card-footer">{{ footer }}</div>
+    <div v-else-if="$slots.footer">
+      <slot name="footer"></slot>
+    </div>
+  </div>
+</template>
+
+<style></style>
