1e14ad5facd3b7cf5ca62e8a4be8ef93bbbffc61
Alexander Brown <kemikalstealth@icloud.com>
2024-02-10 03:09:06 -0600

    Initial project setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: ["~/assets/main.css"],
+  components: ["~/components", "~/components/card"],
+  modules: [
+    "@nuxt/content",
+    "@pinia/nuxt",
+    ["@nuxtjs/color-mode", { colorMode: { classSuffix: "" } }],
+  ],
+});
