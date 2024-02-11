1dc2743e87c17f8235bcef0f4fe22fa6c7cf1efd
Alexander Brown <kemikalstealth@icloud.com>
2024-02-11 16:00:04 -0600

    Added BCardText

--- app.vue
+++ app.vue
@@ -2,13 +2,19 @@
   <div class="d-flex justify-content-center align-items-center mt-5 mx-4">
     <b-card-group>
       <b-card header="Card Header" title="Card Title" sub-title="Card Subtitle" footer="Card Footer">
-        Some quick example text to build on the card title and make up the bulk of the card's content.
+        <b-card-text
+          >Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text
+        >
       </b-card>
       <b-card header="Card Header" title="Card Title" sub-title="Card Subtitle" footer="Card Footer">
-        Some quick example text to build on the card title and make up the bulk of the card's content.
+        <b-card-text
+          >Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text
+        >
       </b-card>
       <b-card header="Card Header" title="Card Title" sub-title="Card Subtitle" footer="Card Footer">
-        Some quick example text to build on the card title and make up the bulk of the card's content.
+        <b-card-text
+          >Some quick example text to build on the card title and make up the bulk of the card's content.</b-card-text
+        >
       </b-card>
     </b-card-group>
   </div>
---
+++ components/card/BCardText.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="textTag" class="card-text">
+    <slot></slot>
+  </component>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  textTag: {
+    type: String,
+    default: "p",
+  },
+});
+</script>
+
+<style></style>
