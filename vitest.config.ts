1fb25fa10c13c7818a4efd87c4b56ae62af86037
Alexander Brown <kemikalstealth@icloud.com>
2024-02-11 00:16:29 -0600

    trying to get ci test to work

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineVitestConfig } from '@nuxt/test-utils/config';
 export default defineVitestConfig({
   // any custom Vitest config you require
   test: {
-    environment: 'nuxt',
+    environment: 'jsdom',
   },
 });
