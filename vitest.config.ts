7f146ff3c257a72defe9cc53218cbecf59348ffd
Alexander Brown <kemikalstealth@icloud.com>
2024-02-11 00:15:14 -0600

    updated vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,8 @@
-import { defineVitestConfig } from '@nuxt/test-utils/config'
+import { defineVitestConfig } from '@nuxt/test-utils/config';
 
 export default defineVitestConfig({
   // any custom Vitest config you require
   test: {
     environment: 'nuxt',
-    globals: true,
-    // Specify the paths to your test files
-    include: ['test/**/*.test.ts']
-  }
-})
+  },
+});
