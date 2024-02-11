8ab5adf47b2eb20a5ecd9cf0be2bf1d8d638aba9
Alexander Brown <kemikalstealth@icloud.com>
2024-02-11 15:57:46 -0600

    Added BCardGroup

--- app.vue
+++ app.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="d-flex justify-content-center align-items-center mt-5 mx-4">
-    <b-card
-      header="Card Header"
-      title="Card Title"
-      sub-title="Card Subtitle"
-      footer="Card Footer"
-    >
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </b-card>
+    <b-card-group>
+      <b-card header="Card Header" title="Card Title" sub-title="Card Subtitle" footer="Card Footer">
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+      </b-card>
+      <b-card header="Card Header" title="Card Title" sub-title="Card Subtitle" footer="Card Footer">
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+      </b-card>
+      <b-card header="Card Header" title="Card Title" sub-title="Card Subtitle" footer="Card Footer">
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+      </b-card>
+    </b-card-group>
   </div>
 </template>
 
---
+++ components/card/BCardGroup.vue
@@ -0,0 +1,16 @@
+<template>
+  <component :is="tag" class="card-group">
+    <slot></slot>
+  </component>
+</template>
+<!-- TODO: Add grid view as an option, will need to add num-max-columns and num-max-rows -->
+<script lang="ts" setup>
+defineProps({
+  tag: {
+    type: String,
+    default: 'div',
+  },
+});
+</script>
+
+<style></style>
