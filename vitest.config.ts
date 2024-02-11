910fa43a743f8524e2175a17ed12bfb6e59983e7
Alexander Brown <kemikalstealth@icloud.com>
2024-02-11 01:09:03 -0600

    Set up tests. Fixed linting

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,9 @@
 import { defineVitestConfig } from '@nuxt/test-utils/config';
 
-export default defineVitestConfig({});
+export default defineVitestConfig({
+  // any custom Vitest config you require
+  test: {
+    environment: 'nuxt',
+    globals: true,
+  },
+});
