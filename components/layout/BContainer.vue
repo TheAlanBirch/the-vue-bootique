f323a2445b093edf9bacff9e2595dd4d2ae397d2
Alexander Brown <kemikalstealth@icloud.com>
2024-02-14 01:45:23 -0600

    Added BContainer

---
+++ components/layout/BContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="tag" :class="containerClasses">
+    <slot></slot>
+  </component>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  fluid: {
+    type: [Boolean, String],
+    default: false,
+  },
+  tag: {
+    type: String,
+    default: 'div',
+  },
+});
+
+const containerClasses = computed(() => {
+  if (props.fluid) return typeof props.fluid === 'boolean' ? 'container-fluid' : `container-${props.fluid}`;
+  return 'container';
+});
+</script>
+
+<style></style>
