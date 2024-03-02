57100b2a3ee0a776e2a2ab86a7366344a4d9e885
Alexander Brown <kemikalstealth@icloud.com>
2024-03-02 15:13:18 -0600

    Updating exhibit

--- pages/list-group-exhibit.vue
+++ pages/list-group-exhibit.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="d-flex flex-column justify-content-center align-items-center my-5 mx-4 gap-5">
-    <div class="row w-100">
+    <div class="row justify-content-between w-100">
       <div class="col">
         <b-list-group class="w-100" style="max-width: 400px">
           <b-list-group-item href="#">Cras justo odio</b-list-group-item>
@@ -14,7 +14,7 @@
           <b-list-group-item active>Morbi leo risus</b-list-group-item>
         </b-list-group>
       </div>
-      <div class="col">
+      <div class="col align-items-center d-flex flex-column justify-content-between">
         <b-list-group class="w-100" style="max-width: 400px">
           <b-list-group-item>Default list group item</b-list-group-item>
           <b-list-group-item variant="primary">Primary list group item</b-list-group-item>
@@ -27,7 +27,7 @@
           <b-list-group-item variant="dark">Dark list group item</b-list-group-item>
         </b-list-group>
       </div>
-      <div class="col">
+      <div class="col align-items-end d-flex flex-column justify-content-between">
         <b-list-group class="w-100" style="max-width: 400px">
           <b-list-group-item href="#">Default list group item</b-list-group-item>
           <b-list-group-item href="#" variant="primary">Primary list group item</b-list-group-item>
